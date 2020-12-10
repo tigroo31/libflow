ab55ae73e06581522823639af65b7b5538d3ad47
Frédéric Gardes <frederic.gardes@orange.com>
2020-12-10 10:05:29 +0100

    flow: fix the particular Eq and Hash implementation (value switch allowed)
    Add Doc comments.
    Add unit tests.
    Manage backward and forward flows.

--- src/flow/flow_id.rs
+++ src/flow/flow_id.rs
@@ -1,8 +1,18 @@
-use serde::Serialize;
 use std::cmp::Ordering::{Equal, Greater, Less};
 use std::hash::{Hash, Hasher};
 use std::net::{IpAddr, Ipv4Addr};
+use std::str::FromStr;
 
+use serde::Serialize;
+
+/// The flow unique identifier.
+/// A flow id is equal to
+/// another flow with the same
+/// transport protocol, src IP and port and dest IP and port.
+/// A flow id is also equal to
+/// another flow with the same
+/// transport protocol, and the src IP and port from one
+/// equal to the dest IP and port of the other.
 #[derive(Debug, Serialize)]
 pub struct FlowId {
     /// Layer 4 protocol (e.g TCP, UDP, ICMP)
@@ -18,6 +28,8 @@ pub struct FlowId {
 }
 
 impl Default for FlowId {
+    /// Create a Flow Id with defaults values
+    /// and "0.0.0.0" IP addresses for src and dest.
     fn default() -> Self {
         Self {
             transport_protocol: 0,
@@ -34,9 +46,9 @@ impl PartialEq for FlowId {
         // same protocol
         self.transport_protocol == other.transport_protocol &&
             // same source and destination IP and port (backward), so equal
-            (self.src == other.src && self.src_port == other.src_port && self.dst == other.dst && self.dst_port == other.dst_port) ||
+            ((self.src == other.src && self.src_port == other.src_port && self.dst == other.dst && self.dst_port == other.dst_port) ||
             // same reverse source and destination IP and port (forward), so equal too
-            (self.src == other.dst && self.src_port == other.dst_port && self.dst == other.src && self.dst_port == other.dst_port)
+            (self.src == other.dst && self.src_port == other.dst_port && self.dst == other.src && self.dst_port == other.src_port))
     }
 }
 
@@ -66,14 +78,132 @@ impl Hash for FlowId {
 }
 
 impl FlowId {
-    pub fn new() -> Self {
-        FlowId { ..Default::default() }
+    /// Create a Flow Id with IP addresses for src and dest provided as string.
+    pub fn new(transport_protocol: u8, src: &str, dst: &str, src_port: u16, dst_port: u16) -> Self {
+        let src_ip_addr = IpAddr::from_str(src).unwrap();
+        let dst_ip_addr = IpAddr::from_str(dst).unwrap();
+
+        FlowId {
+            transport_protocol,
+            src: src_ip_addr,
+            dst: dst_ip_addr,
+            src_port,
+            dst_port,
+        }
     }
 }
 
-#[test]
-fn test_new() {
-    let information = FlowId::new();
-    // TODO implement tests
-    assert_eq!(FlowId { ..Default::default() }, information)
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr};
+
+    use crate::flow::flow_id::FlowId;
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::{Hash, Hasher};
+
+    fn buil_local_flow() -> FlowId {
+        FlowId::new(
+            17, // UDP
+            "127.0.0.1",
+            "192.168.0.1",
+            8001,
+            8002,
+        )
+    }
+
+    #[test]
+    fn test_default() {
+        let default = FlowId::default();
+        assert_eq!(default.transport_protocol, 0);
+        assert_eq!(default.src, IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)));
+        assert_eq!(default.dst, IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)));
+        assert_eq!(default.src_port, 0);
+        assert_eq!(default.dst_port, 0);
+    }
+
+    #[test]
+    fn test_local_new() {
+        let new = buil_local_flow();
+        assert_eq!(new.transport_protocol, 17);
+        assert_eq!(new.src, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
+        assert_eq!(new.dst, IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1)));
+        assert_eq!(new.src_port, 8001);
+        assert_eq!(new.dst_port, 8002);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_src_new() {
+        FlowId::new(
+            17, // UDP
+            "bad",
+            "192.168.0.1",
+            8001,
+            8002,
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_dst_new() {
+        FlowId::new(
+            17, // UDP
+            "127.0.0.1",
+            "192.168.0.KK",
+            8001,
+            8002,
+        );
+    }
+
+    #[test]
+    fn test_forward_eq() {
+        let flow1 = buil_local_flow();
+        // the same
+        let flow2 = buil_local_flow();
+        assert_eq!(flow1, flow2)
+    }
+
+    #[test]
+    fn test_backward_eq() {
+        let flow1 = buil_local_flow();
+        // the reverse
+        let flow2 = FlowId::new(
+            17, // UDP
+            "192.168.0.1",
+            "127.0.0.1",
+            8002,
+            8001,
+        );
+        assert_eq!(flow1, flow2)
+    }
+
+    #[test]
+    fn test_forward_hash() {
+        let mut hasher1 = DefaultHasher::new();
+        let flow1 = buil_local_flow();
+        flow1.hash(&mut hasher1);
+        // the same
+        let mut hasher2 = DefaultHasher::new();
+        let flow2 = buil_local_flow();
+        flow2.hash(&mut hasher2);
+        assert_eq!(hasher1.finish(), hasher2.finish());
+    }
+
+    #[test]
+    fn test_backward_hash() {
+        let mut hasher1 = DefaultHasher::new();
+        let flow1 = buil_local_flow();
+        flow1.hash(&mut hasher1);
+        // the reverse
+        let mut hasher2 = DefaultHasher::new();
+        let flow2 = FlowId::new(
+            17, // UDP
+            "192.168.0.1",
+            "127.0.0.1",
+            8002,
+            8001,
+        );
+        flow2.hash(&mut hasher2);
+        assert_eq!(hasher1.finish(), hasher2.finish());
+    }
 }
--- src/flow/mod.rs
+++ src/flow/mod.rs
@@ -1,4 +1,5 @@
 mod flag;
 pub mod flow_id;
+pub mod flow_information;
 pub mod information;
 mod statistic;
