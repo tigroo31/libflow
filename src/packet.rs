68b0d074150bbc2b66d3872b0c80432c896fae1a
Frédéric Gardes <frederic.gardes@orange.com>
2021-01-15 11:30:47 +0100

    packet: add the network header length
    Use alpha ordered flag list.
    Add unit tests.

--- src/packet.rs
+++ src/packet.rs
@@ -1,20 +1,23 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 use std::time::Duration;
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use crate::flag::Flag;
 
-#[derive(Debug, Default, Serialize)]
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Packet {
     // number of bytes
     pub length: u64,
     /// timestamp
     pub timestamp: Duration,
     /// list of flags
-    pub flag_list: HashSet<Flag>,
+    pub flag_list: BTreeSet<Flag>,
     /// layer 3 protocol (e.g IPv4, IPv6)
     pub network_protocol: u16,
+    /// layer 3 header size
+    pub network_header_length: Option<usize>,
     // position into the set considered
     pub position: usize,
 }
@@ -22,24 +25,108 @@ pub struct Packet {
 impl Packet {
     /// Provide the default structure for now.
     pub fn new() -> Self {
-        Packet { ..Default::default() }
+        Self { ..Default::default() }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
+    use std::collections::BTreeSet;
     use std::time::Duration;
 
     use crate::packet::Packet;
+
+    fn remove_whitespace(s: &str) -> String {
+        s.split_whitespace().collect()
+    }
+
+    fn basic_packet() -> &'static str {
+        return r#"
+{
+  "length": 66,
+  "timestamp": {
+    "secs": 1595325117,
+    "nanos": 502092000
+  },
+  "flag_list": [],
+  "network_protocol": 34525,
+  "network_header_length": 5,
+  "position": 28456
+}
+"#;
+    }
+
+    fn complete_packet() -> &'static str {
+        return r#"
+{
+  "length": 55,
+  "timestamp": {
+    "secs": 1595325118,
+    "nanos": 502092010
+  },
+  "flag_list": ["ACK","CWR","ECE","FIN","NS","PSH","RST","SYN","URG"],
+  "network_protocol": 17,
+  "network_header_length": 5,
+  "position": 1234
+}
+"#;
+    }
+
+    fn bad_packet_with_string_network_protocol() -> &'static str {
+        return r#"
+{
+  "length": 44,
+  "timestamp": {
+    "secs": 1595325119,
+    "nanos": 502092020
+  },
+  "flag_list": [],
+  "network_protocol": "6",
+  "network_header_length": 10,
+  "position": 2345
+}
+"#;
+    }
+
+    fn bad_packet_with_u32_position() -> &'static str {
+        return r#"
+{
+  "length": 33,
+  "timestamp": {
+    "secs": 1595325120,
+    "nanos": 502092030
+  },
+  "flag_list": [],
+  "network_protocol": 6,
+  "network_header_length": 7,
+  "position": 42424242424242424242
+}
+"#;
+    }
+
+    fn bad_packet_without_flag_list() -> &'static str {
+        return r#"
+{
+  "length": 23,
+  "timestamp": {
+    "secs": 1595325130,
+    "nanos": 502092031
+  },
+  "network_protocol": 6,
+  "network_header_length": 7,
+  "position": 42
+}
+"#;
+    }
 
     #[test]
     fn test_default() {
         let default = Packet::default();
         assert_eq!(default.length, 0);
         assert_eq!(default.timestamp, Duration::default());
-        assert_eq!(default.flag_list, HashSet::default());
+        assert_eq!(default.flag_list, BTreeSet::default());
         assert_eq!(default.network_protocol, 0);
+        assert_eq!(default.network_header_length, None);
         assert_eq!(default.position, 0);
     }
 
@@ -48,8 +135,46 @@ mod tests {
         let new = Packet::new();
         assert_eq!(new.length, 0);
         assert_eq!(new.timestamp, Duration::default());
-        assert_eq!(new.flag_list, HashSet::default());
+        assert_eq!(new.flag_list, BTreeSet::default());
         assert_eq!(new.network_protocol, 0);
+        assert_eq!(new.network_header_length, None);
         assert_eq!(new.position, 0);
+    }
+
+    #[test]
+    fn it_can_deserialize_then_serialize_a_basic_packet() {
+        let json = basic_packet();
+        let packet: Packet = serde_json::from_str(json).unwrap();
+        assert_eq!(packet.timestamp.as_nanos(), 1595325117502092000);
+        assert_eq!(serde_json::to_string(&packet).unwrap(), remove_whitespace(json));
+    }
+
+    #[test]
+    fn it_can_deserialize_then_serialize_a_complete_packet() {
+        let json = complete_packet();
+        let packet: Packet = serde_json::from_str(json).unwrap();
+        assert_eq!(packet.timestamp.as_nanos(), 1595325118502092010);
+        assert_eq!(serde_json::to_string(&packet).unwrap(), remove_whitespace(json));
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_should_panic_when_deserializing_a_packet_with_u32_position() {
+        let json = bad_packet_with_u32_position();
+        let _: Packet = serde_json::from_str(json).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_should_panic_when_deserializing_a_packet_with_string_network() {
+        let json = bad_packet_with_string_network_protocol();
+        let _: Packet = serde_json::from_str(json).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_should_panic_when_deserializing_a_packet_without_flag_list() {
+        let json = bad_packet_without_flag_list();
+        let _: Packet = serde_json::from_str(json).unwrap();
     }
 }
